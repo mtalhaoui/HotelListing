fe4f237380554abc75f8b5e738fa0827d2b4634b
mtalhaoui <mohammed.talhaoui@hotmail.com>
2021-12-28 16:12:26 +0100

    Updated swagger url

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -60,7 +60,7 @@ namespace HotelListing
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
 
-            services.AddControllers(config => 
+            services.AddControllers(config =>
             {
                 config.CacheProfiles.Add("120SecondsDuration", new CacheProfile
                 {
@@ -77,9 +77,14 @@ namespace HotelListing
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HotelListing v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "Hotel Listing API");
+            });
 
             app.ConfigureExceptionHandler();
 
